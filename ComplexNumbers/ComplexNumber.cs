e20a8c6f48fc7f11b3565e04b9bfca3b7a1893f2
Egor Slupchuk <egorjarovit@gmail.com>
2024-04-10 01:06:24 +0300

    Добавьте файлы проекта.

---
+++ ComplexNumbers/ComplexNumber.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumbers
+{
+    public class ComplexNumber
+    {
+        double realPartNumber;
+        double imaginaryPartNumber;
+
+        // Конструкторы
+        public ComplexNumber()
+        {
+            realPartNumber = 0;
+            imaginaryPartNumber = 0;
+        }
+
+        public ComplexNumber( double realPartNumberUser, double imaginaryPartNumberUser) {
+            realPartNumber = realPartNumberUser;
+            imaginaryPartNumber = imaginaryPartNumberUser;
+        }
+
+        // Функция перевода из радиан в градусы
+        private static double RadiansToDegrees(double radians)
+        {
+            return radians * 180 / Math.PI;
+        }
+        // Функция перевода из радиан в градусы
+        private static double DegreesToRadians(double degree)
+        {
+            return degree * Math.PI / 180;
+        }
+
+        //Вычисление корня комплексного числа
+        public static ComplexNumber[] ActionsWithComplexMD(ComplexNumber z1, int n)
+        {
+            double module = 0;
+            if (z1.realPartNumber != 0 && z1.imaginaryPartNumber == 0) module = Math.Abs(z1.realPartNumber);
+            if (z1.realPartNumber == 0 && z1.imaginaryPartNumber != 0) module = Math.Abs(z1.imaginaryPartNumber);
+            else module = Math.Sqrt((z1.realPartNumber * (z1.realPartNumber + z1.imaginaryPartNumber * z1.imaginaryPartNumber)));
+
+            //Считаем аргумент
+            double arg = 0;
+            if (z1.realPartNumber > 0) arg = RadiansToDegrees(Math.Atan(z1.imaginaryPartNumber / z1.realPartNumber));
+            if (z1.realPartNumber < 0 && z1.imaginaryPartNumber >= 0) arg = Math.PI + RadiansToDegrees(Math.Atan(z1.imaginaryPartNumber / z1.realPartNumber));
+            if (z1.realPartNumber < 0 && z1.imaginaryPartNumber < 0) arg = -Math.PI + RadiansToDegrees(Math.Atan(z1.imaginaryPartNumber / z1.realPartNumber));
+            if (z1.realPartNumber == 0 && z1.imaginaryPartNumber > 0) arg = Math.PI / 2;
+            if (z1.realPartNumber == 0 && z1.imaginaryPartNumber < 0) arg = -Math.PI / 2;
+
+            var tmp2 = 0;
+            var tmp3 = 0;
+            ComplexNumber[] root = new ComplexNumber[n];
+
+            // Считаем корни
+            for (int i = 0; i < n; i++)
+            {
+                tmp2 = (int)((Math.Pow(module, 1.0 / n) * Math.Cos(DegreesToRadians((arg + 2.0 * Math.PI * i) / n))) * 1000);
+                tmp3 = (int)((Math.Pow(module, 1.0 / n) * Math.Sin(DegreesToRadians((arg + 2.0 * Math.PI * i) / n))) * 1000);
+
+                root[i] = new ComplexNumber(tmp2, tmp3);
+            }
+            return root;
+        }
+       
+        //Вывод корней
+        public static string DerivationRoots(ComplexNumber[] root, int n)
+        {
+            string resultString = "";
+            for (int i = 0; i < n; i++)
+            {
+                if (root[i].imaginaryPartNumber >= 0)
+                {
+                    resultString += "W" + i + "=" + (root[i].realPartNumber) / 1000
+                        + " + " + (root[i].imaginaryPartNumber) / 1000 + "i" + "\n";
+                }
+                else
+                {
+                    resultString += "W" + i + "=" + (root[i].realPartNumber) / 1000
+                       + (root[i].imaginaryPartNumber) / 1000 + "i" + "\n";
+                }
+
+            }
+            return resultString;
+        }
+
+        // Метод вывода комплексного числа
+        public static string ComplexNumberOutput(ComplexNumber z)
+        {
+            string resultComplexString = "";
+            if (z.imaginaryPartNumber > 0)
+            {
+                resultComplexString += "z = " +
+                z.realPartNumber + " + " +
+                z.imaginaryPartNumber + "i";
+                resultComplexString += "\nТригонометрическая форма: " + TrigonometricView(z);
+
+            }
+            else
+            {
+                resultComplexString += "z = " +
+                z.realPartNumber +
+                z.imaginaryPartNumber + "i";
+                resultComplexString += "\nТригонометрическая форма: " + TrigonometricView(z);
+            }
+            return resultComplexString;
+
+        }
+        //Сложение комплексных чисел
+        public static ComplexNumber ActionsWithComplex(ComplexNumber z1, ComplexNumber z2)
+        {
+            //
+            double newRealPart = z1.realPartNumber + z2.realPartNumber;
+            double newImaginaryPart = z1.imaginaryPartNumber + z2.imaginaryPartNumber;
+            return new ComplexNumber(newRealPart, newImaginaryPart);// создание нового объекта класса
+        }
+
+        //Вычитание комплексных чисел
+        public static ComplexNumber ActionsWithComplexSub(ComplexNumber z1, ComplexNumber z2)
+        {
+            double newRealPart = z1.realPartNumber - z2.realPartNumber;
+            double newImaginaryPart = z1.imaginaryPartNumber - z2.imaginaryPartNumber;
+            return new ComplexNumber(newRealPart, newImaginaryPart);// создание нового объекта класса
+        }
+
+        //Умножение комплексных чисел MD - Multiplication, division - умножение, деление
+        public static ComplexNumber ActionsWithComplexMD(ComplexNumber z1, ComplexNumber z2)
+        {
+            double newRealPart = z1.realPartNumber * z2.realPartNumber - z1.imaginaryPartNumber * z2.imaginaryPartNumber;
+            double newImaginaryPart = z1.realPartNumber * z2.imaginaryPartNumber + z2.realPartNumber * z1.imaginaryPartNumber;
+            return new ComplexNumber(newRealPart, newImaginaryPart);// создание нового объекта класса
+        }
+        //Деление комплексных чисел MD - Multiplication, division - умножение, деление
+        public static ComplexNumber ActionsWithComplexMDDiv(ComplexNumber z1, ComplexNumber z2)
+        {
+            double numerator_one = z1.realPartNumber * z2.realPartNumber + z1.imaginaryPartNumber * z2.imaginaryPartNumber;
+            double numeratoe_two = z2.realPartNumber * z1.imaginaryPartNumber - z1.realPartNumber * z2.imaginaryPartNumber;
+            double denominator = Math.Pow(z2.realPartNumber, 2) + Math.Pow(z2.imaginaryPartNumber, 2);
+            return new ComplexNumber(numerator_one / (denominator), numeratoe_two / (denominator));
+        }
+
+        // Приведение комплексного числа к тригонометрическому виду
+        public static string TrigonometricView(ComplexNumber z1)
+        {
+            double module = 0;
+            if (z1.realPartNumber != 0 && z1.imaginaryPartNumber == 0) module = Math.Abs(z1.realPartNumber);
+            if (z1.realPartNumber == 0 && z1.imaginaryPartNumber != 0) module = Math.Abs(z1.imaginaryPartNumber);
+            else module = Math.Round(Math.Sqrt((z1.realPartNumber * (z1.realPartNumber + z1.imaginaryPartNumber * z1.imaginaryPartNumber))), 3);
+
+            //Считаем аргумент
+            double arg = 0;
+            if (z1.realPartNumber > 0) arg = RadiansToDegrees(Math.Atan(z1.imaginaryPartNumber / z1.realPartNumber));
+            if (z1.realPartNumber < 0 && z1.imaginaryPartNumber >= 0) arg = Math.PI + RadiansToDegrees(Math.Atan(z1.imaginaryPartNumber / z1.realPartNumber));
+            if (z1.realPartNumber < 0 && z1.imaginaryPartNumber < 0) arg = -Math.PI + RadiansToDegrees(Math.Atan(z1.imaginaryPartNumber / z1.realPartNumber));
+            if (z1.realPartNumber == 0 && z1.imaginaryPartNumber > 0) arg = Math.PI / 2;
+            if (z1.realPartNumber == 0 && z1.imaginaryPartNumber < 0) arg = -Math.PI / 2;
+
+            string resultStringTrigonometric = "";
+            resultStringTrigonometric = module + " * " + "(cos(" + Math.Round(arg, 3) + ") - isin(" +Math.Round(arg, 3) +"))";
+            return resultStringTrigonometric;    
+
+        }
+
+    }
+}
---
+++ ComplexNumbers/MainWindow.xaml.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComplexNumbers
+{
+    
+    public partial class MainWindow : Window
+    {
+        double realPartFirst; // Действительная часть первого числа
+        double imaginaryPartFirst; // Мнимая часть первого числа
+        int rootDegree;  // Степень корня первого комплексного числа
+
+        double realPartSecond; // Действительная часть первого числа
+        double imaginaryPartSecond; // Мнимая часть первого числа
+        int rootDegreeSecond;  // Степень корня первого комплексного числа
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        // Метод для подсчёта корней комплексных чисел
+        private void Roots_Click(object sender, RoutedEventArgs e)
+        {
+            ComplexNumber complexFirst = new ComplexNumber();
+            ComplexNumber complexSecond = new ComplexNumber();
+            bool tmp = false;
+            bool tmpTwo = false;
+            try
+            {
+                if (CofReal.Text !="" || CofImaginary.Text != "")
+                {
+                    realPartFirst = Convert.ToDouble(CofReal.Text);
+                    imaginaryPartFirst = Convert.ToDouble(CofImaginary.Text);
+                    rootDegree = Convert.ToInt32(CofRootDegree.Text);
+                    complexFirst = new ComplexNumber(realPartFirst, imaginaryPartFirst);
+                    tmp = true;
+                }
+                
+                if (CofRealSecond.Text != "" || CofImaginarySecond.Text != ""){
+                    realPartSecond = Convert.ToDouble(CofRealSecond.Text);
+                    imaginaryPartSecond = Convert.ToDouble(CofImaginarySecond.Text);
+                    rootDegreeSecond = Convert.ToInt32(CofRootDegreeSecond.Text);
+                    complexSecond = new ComplexNumber(realPartSecond, imaginaryPartSecond);
+                    tmpTwo = true;
+                }               
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный ввод данных!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (tmp)
+            {
+                // Подсчёт корней первого числа
+                ComplexNumber[] massiveRoots = ComplexNumber.ActionsWithComplexMD(complexFirst, rootDegree);
+                string resultReceiver = ComplexNumber.DerivationRoots(massiveRoots, rootDegree);
+                AnswerBlock.Text += "\nКорни первого комплексного числа:\n" + resultReceiver + "\n";
+                if (tmpTwo == false) { AnswerBlock.Text += "Второе комплексное число введено не было!\n"; }
+            }
+            if (tmpTwo)
+            {
+                // Подчсчёт корней второго числа
+                ComplexNumber[] massiveRootsSecond = ComplexNumber.ActionsWithComplexMD(complexSecond, rootDegree);
+                string resultReceiverSecond = ComplexNumber.DerivationRoots(massiveRootsSecond, rootDegreeSecond);
+
+                AnswerBlock.Text += "\nКорни второго комплексного числа:\n" + resultReceiverSecond + "\n";
+                if (tmp == false) { AnswerBlock.Text += "Первое комплексное число введено не было!\n"; }
+            }
+            if (tmp == false && tmpTwo == false) { MessageBox.Show("Операция невозможна так как вы не ввели все комплексные числа!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error); }
+           
+
+        }
+        // Метод для работы со сложением двух компелксных чисел
+        private void Addition_Click(object sender, RoutedEventArgs e)
+        {
+            ComplexNumber complexFirst = new ComplexNumber();
+            ComplexNumber complexSecond = new ComplexNumber();
+            bool tmp = false;
+            bool tmpTwo = false;
+            try
+            {
+                if (CofReal.Text != "" || CofImaginary.Text != "")
+                {
+                    realPartFirst = Convert.ToDouble(CofReal.Text);
+                    imaginaryPartFirst = Convert.ToDouble(CofImaginary.Text);
+                    complexFirst = new ComplexNumber(realPartFirst, imaginaryPartFirst);
+                    tmp = true;
+                }
+
+                if (CofRealSecond.Text != "" || CofImaginarySecond.Text != "")
+                {
+                    realPartSecond = Convert.ToDouble(CofRealSecond.Text);
+                    imaginaryPartSecond = Convert.ToDouble(CofImaginarySecond.Text);
+                    complexSecond = new ComplexNumber(realPartSecond, imaginaryPartSecond);
+                    tmpTwo = true;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный ввод данных!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (tmp && tmpTwo)
+            {
+                var complexResult = ComplexNumber.ActionsWithComplex(complexFirst, complexSecond);
+                AnswerBlock.Text += "\nРезультат сложения комплексных чисел:\n" + ComplexNumber.ComplexNumberOutput(complexResult);
+            }
+            else
+            {
+                MessageBox.Show("Операция невозможна так как вы не ввели все комплексные числа!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+        // Метод для работы с вычитанием компелксных чисел
+        private void Subtraction_Click(object sender, RoutedEventArgs e)
+        {
+            ComplexNumber complexFirst = new ComplexNumber();
+            ComplexNumber complexSecond = new ComplexNumber();
+            bool tmp = false;
+            bool tmpTwo = false;
+            try
+            {
+                if (CofReal.Text != "" || CofImaginary.Text != "")
+                {
+                    realPartFirst = Convert.ToDouble(CofReal.Text);
+                    imaginaryPartFirst = Convert.ToDouble(CofImaginary.Text);
+                    complexFirst = new ComplexNumber(realPartFirst, imaginaryPartFirst);
+                    tmp = true;
+                }
+
+                if (CofRealSecond.Text != "" || CofImaginarySecond.Text != "")
+                {
+                    realPartSecond = Convert.ToDouble(CofRealSecond.Text);
+                    imaginaryPartSecond = Convert.ToDouble(CofImaginarySecond.Text);
+                    complexSecond = new ComplexNumber(realPartSecond, imaginaryPartSecond);
+                    tmpTwo = true;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный ввод данных!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (tmp && tmpTwo)
+            {
+                var complexResult = ComplexNumber.ActionsWithComplexSub(complexFirst, complexSecond);
+                AnswerBlock.Text += "\nРезультат вычитания комплексных чисел:\n" + ComplexNumber.ComplexNumberOutput(complexResult);
+            }
+            else
+            {
+                MessageBox.Show("Операция невозможна так как вы не ввели все комплексные числа!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+        // Метод для работы с умножением двух комплексных чисел
+        private void Multiplication_Click(object sender, RoutedEventArgs e)
+        {
+            ComplexNumber complexFirst = new ComplexNumber();
+            ComplexNumber complexSecond = new ComplexNumber();
+            bool tmp = false;
+            bool tmpTwo = false;
+            try
+            {
+                if (CofReal.Text != "" || CofImaginary.Text != "")
+                {
+                    realPartFirst = Convert.ToDouble(CofReal.Text);
+                    imaginaryPartFirst = Convert.ToDouble(CofImaginary.Text);
+                    complexFirst = new ComplexNumber(realPartFirst, imaginaryPartFirst);
+                    tmp = true;
+                }
+
+                if (CofRealSecond.Text != "" || CofImaginarySecond.Text != "")
+                {
+                    realPartSecond = Convert.ToDouble(CofRealSecond.Text);
+                    imaginaryPartSecond = Convert.ToDouble(CofImaginarySecond.Text);
+                    complexSecond = new ComplexNumber(realPartSecond, imaginaryPartSecond);
+                    tmpTwo = true;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный ввод данных!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (tmp && tmpTwo)
+            {
+                var complexResult = ComplexNumber.ActionsWithComplexMD(complexFirst, complexSecond);
+                AnswerBlock.Text += "\nРезультат умножения комплексных чисел:\n" + ComplexNumber.ComplexNumberOutput(complexResult);
+            }
+            else
+            {
+                MessageBox.Show("Операция невозможна так как вы не ввели все комплексные числа!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        // Метод для работы с делением двух комплексных чисел
+        private void Division_Click(object sender, RoutedEventArgs e)
+        {
+            ComplexNumber complexFirst = new ComplexNumber();
+            ComplexNumber complexSecond = new ComplexNumber();
+            bool tmp = false;
+            bool tmpTwo = false;
+            try
+            {
+                if (CofReal.Text != "" || CofImaginary.Text != "")
+                {
+                    realPartFirst = Convert.ToDouble(CofReal.Text);
+                    imaginaryPartFirst = Convert.ToDouble(CofImaginary.Text);
+                    complexFirst = new ComplexNumber(realPartFirst, imaginaryPartFirst);
+                    tmp = true;
+                }
+
+                if (CofRealSecond.Text != "" || CofImaginarySecond.Text != "")
+                {
+                    realPartSecond = Convert.ToDouble(CofRealSecond.Text);
+                    imaginaryPartSecond = Convert.ToDouble(CofImaginarySecond.Text);
+                    complexSecond = new ComplexNumber(realPartSecond, imaginaryPartSecond);
+                    tmpTwo = true;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный ввод данных!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (tmp && tmpTwo)
+            {
+                var complexResult = ComplexNumber.ActionsWithComplexMDDiv(complexFirst, complexSecond);
+                AnswerBlock.Text += "\nРезультат деления комплексных чисел:\n" + ComplexNumber.ComplexNumberOutput(complexResult);
+            }
+            else
+            {
+                MessageBox.Show("Операция невозможна так как вы не ввели все комплексные числа!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        // Метод для отчищения (души) TextBox, в котором содержится ответ
+        private void CleanButton_Click(object sender, RoutedEventArgs e)
+        {
+            CofReal.Clear();
+            CofImaginary.Clear();
+            CofRealSecond.Clear();
+            CofImaginarySecond.Clear();
+            CofRootDegree.Clear();
+            CofRootDegreeSecond.Clear();
+            AnswerBlock.Clear();
+        }
+
+        private void TrigonometricForm_Click(object sender, RoutedEventArgs e)
+        {
+            ComplexNumber complexFirst = new ComplexNumber();
+            ComplexNumber complexSecond = new ComplexNumber();
+            bool tmp = false;
+            bool tmpTwo = false;
+            try
+            {
+                if (CofReal.Text != "" || CofImaginary.Text != "")
+                {
+                    realPartFirst = Convert.ToDouble(CofReal.Text);
+                    imaginaryPartFirst = Convert.ToDouble(CofImaginary.Text);
+                    complexFirst = new ComplexNumber(realPartFirst, imaginaryPartFirst);
+                    tmp = true;
+                }
+
+                if (CofRealSecond.Text != "" || CofImaginarySecond.Text != "")
+                {
+                    realPartSecond = Convert.ToDouble(CofRealSecond.Text);
+                    imaginaryPartSecond = Convert.ToDouble(CofImaginarySecond.Text);
+                    complexSecond = new ComplexNumber(realPartSecond, imaginaryPartSecond);
+                    tmpTwo = true;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный ввод данных!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (tmp)
+            {
+                AnswerBlock.Text += "\nТригонометрическая форма первого комплексного числа:\n " + ComplexNumber.TrigonometricView(complexFirst);
+                if (!tmpTwo) { AnswerBlock.Text += "\nВторое комплексное число не указано!"; }
+            }
+            if (tmpTwo)
+            {
+                AnswerBlock.Text += "\nТригонометрическая форма второго комплексного числа:\n " + ComplexNumber.TrigonometricView(complexSecond);
+                if (!tmp) { AnswerBlock.Text += "\nПервое комплексное число не указано!"; }
+            }
+                
+
+            
+            
+        }
+    }
+}
